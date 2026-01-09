085b118794dde86ba779b9bc1ab3c190c8c6600f
jssun <59641485@qq.com>
2026-01-10 01:11:27 +0800

    Chapter 1: class string, shallow copy, and deep copy

--- main.cpp
+++ main.cpp
@@ -24,6 +24,15 @@ namespace W
 			// 正确的 strcpy_s 用法：strcpy_s(目标, 目标缓冲区大小, 源)
 			strcpy(_str, str);
 		}
+		//< 深拷贝构造函数
+		//深拷贝确认了每个对象都有独立的内存空间
+		string(const string& s)
+		{
+			_size = s._size;
+			_capacity = s._capacity;
+			_str = new char[_capacity + 1];//分配新的内存
+			strcpy(_str, s._str);//复制字符串内容
+		}
 		//< 析构函数
 		~string()
 		{
@@ -46,10 +55,25 @@ namespace W
 
 	};
 }
+/**
+* @param 浅拷贝和深拷贝
+* 当通过另一个string对象来构造新的对象，默认会发生浅拷贝，对象共享
+* 同一块内存。当对象被销毁时，会导致多个对象试图释放同一块内存，进而
+* 导致程序崩溃
+*/
+void TestString()
+{
+	W::string s1("asdfasdf");
+	W::string s2(s1);//浅拷贝 s1 s2共享同一块内存 深拷贝都有独立内存
+	//析构函数会尝试两次释放同一块内存，导致程序崩溃
+
+	//析构的顺序：后构造的先析构，调试的话s2先析构然后是s1
+}
 
 int main() 
 {
 	W::string s("fuck you c++");
+	TestString();
 	std::cout << s.c_str() << std::endl;
 	return 0;
 }
